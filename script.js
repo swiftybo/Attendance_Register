03ebe9ffba29a248d6d220e2759d88bfa4c88af2
swiftybo <leungjordan28@gmail.com>
2025-05-07 19:24:13 +0100

    Created register list of objects which contains all the information of the expected attendees.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let register = [
+  { firstName: "John", lastName: "Doe", yearOfBirth: 1979 },
+  { firstName: "Ian", lastName: "Foot", yearOfBirth: 1988 },
+  { firstName: "Steven", lastName: "Talks", yearOfBirth: 1992 },
+  { firstName: "Barty", lastName: "Hall", yearOfBirth: 2003 },
+  { firstName: "Clarity", lastName: "Farrow", yearOfBirth: 1995 },
+  { firstName: "Emily", lastName: "Chives", yearOfBirth: 1955 },
+  { firstName: "Steven", lastName: "Row-Fern", yearOfBirth: 1988 },
+];
